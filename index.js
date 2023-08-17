5dea5f4062530c2bfc417f7ee6561a5d20946ac0
Rishita Gusain <99330383+Rishita-Gusain@users.noreply.github.com>
2023-08-17 19:58:54 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,63 +1,43 @@
+var randomNumber1= Math.floor(Math.random()*6)+1;
 
-var numberofdrumbutton= document.querySelectorAll(".drum").length;
-
-
-for(var i=0;i<numberofdrumbutton;i++){
-document.querySelectorAll(".drum")[i].addEventListener("click",function(){
-   var buttonInnerHTML= this.innerHTML;
-   makeSound(buttonInnerHTML);
-   buttonAnimation(buttonInnerHTML);
-});
-
+if(randomNumber1===1){
+    document.querySelector(".img1").setAttribute("src","./images/dice1.png");
+}else if(randomNumber1===2){
+    document.querySelector(".img1").setAttribute("src","./images/dice2.png");
+}else if(randomNumber1===3){
+    document.querySelector(".img1").setAttribute("src","./images/dice3.png");
 }
-document.addEventListener("keypress",function(event){
-    makeSound(event.key);
-    buttonAnimation(event.key);
+else if(randomNumber1===4){
+    document.querySelector(".img1").setAttribute("src","./images/dice4.png");
+}else if(randomNumber1===5){
+    document.querySelector(".img1").setAttribute("src","./images/dice5.png");
+}else{
+    document.querySelector(".img1").setAttribute("src","./images/dice6.png");
 }
-);
-function makeSound(key){
 
-    
-    switch(key){
-        case "w":
-            var tom1= new Audio("sounds/tom-1.mp3");
-            tom1.play();
-            break;
-        case "a":
-            var tom2=new Audio("sounds/tom-2.mp3");
-            tom2.play();
-            break;
-        case "s":
-            var tom3=new Audio("sounds/tom-3.mp3");
-            tom3.play();
-            break;
-        case "d":
-            var tom4=new Audio("sounds/tom-4.mp3");
-            tom4.play();
-            break;
-        case "j":
-            var snare=new Audio("sounds/snare.mp3");
-            snare.play();
-            break;
-        case "k":
-            var crash=new Audio("sounds/crash.mp3");
-            crash.play();
-            break;
-        case "l":
-            var kick=new Audio("sounds/kick-bass.mp3");
-            kick.play();
-            break;
-        default:
-            console.log(buttonInnerHTML);
+var randomNumber2= Math.floor(Math.random()*6)+1;
+
+if(randomNumber2===1){
+    document.querySelector(".img2").setAttribute("src","./images/dice1.png");
+}else if(randomNumber2===2){
+    document.querySelector(".img2").setAttribute("src","./images/dice2.png");
+}else if(randomNumber2===3){
+    document.querySelector(".img2").setAttribute("src","./images/dice3.png");
+}
+else if(randomNumber2===4){
+    document.querySelector(".img2").setAttribute("src","./images/dice4.png");
+}else if(randomNumber2===5){
+    document.querySelector(".img2").setAttribute("src","./images/dice5.png");
+}else{
+    document.querySelector(".img2").setAttribute("src","./images/dice6.png");
+}
+
+if(randomNumber1>randomNumber2){
+document.querySelector("h1").innerHTML="Player 1 wins";
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 wins";
     }
-
-}
-
-
-function buttonAnimation(currentkey){
-   var activeButton= document.querySelector("."+currentkey);
-   activeButton.classList.add("pressed");
-   setTimeout(function(){
-    activeButton.classList.remove("pressed");
-   },100)
-}
+    else{
+        document.querySelector("h1").innerHTML="Tie Bro";
+    }
